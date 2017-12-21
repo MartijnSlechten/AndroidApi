c25bc1df8ea7c14b919f53f0f0e42c06b542c1ec
Axel Pauwels <axelpauwels.be@gmail.com>
2017-12-21 12:44:08 +0100

    fixing: show download-app-button only when logged in

--- application/views/home.php
+++ application/views/home.php
@@ -7,7 +7,14 @@
 <br>
 <!--    <a href="--><?php //echo site_url('/home/testData') ?><!--"><button type="button" class="btn btn-default">generate metingen</button></a>-->
 <p>
-    <a href="http://axelpauwels.onthewifi.com/androidApp/P2_7_MartijnAxel.apk">
-        <button type="button" class="btn btn-default">Download de App</button>
-    </a>
+    <?php
+    if (!$user == null) {
+   ?>
+        <a href="http://axelpauwels.onthewifi.com/androidApp/P2_7_MartijnAxel.apk">
+            <button type="button" class="btn btn-default"><i style="color: green" class="fa fa-android" aria-hidden="true"></i> Download de app</button>
+        </a>
+    <?php
+    }
+    ?>
+
 </p>
